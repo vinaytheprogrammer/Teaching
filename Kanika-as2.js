bc5ab5a11d7b68d10004526b7ea6d1c0a489e209
Kanika <kanikasunaria@gmail.com>
2024-07-01 15:33:42 +0530

    Kanika's assigment 2

---
+++ Kanika-as2.js
@@ -0,0 +1,33 @@
+// Perform basic arithmetic operations
+let num1 = 10;
+let num2 = 5;
+
+// Addition
+let sum = num1 + num2;
+
+// Subtraction
+let difference = num1 - num2;
+
+// Multiplication
+let product = num1 * num2;
+
+// Division (using division operator)
+let division = num1 / num2;
+
+// Concatenate strings
+let str1 = "Hello";
+let str2 = "World";
+let concatenatedString = str1 + " " + str2;
+
+// Use a template literal
+let name = "Kanika";
+let age = 20;
+let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+
+// Print the results
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Division:", division);
+console.log("Concatenated string:", concatenatedString);
+console.log(greeting);
