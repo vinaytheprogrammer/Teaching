30b5689ba5daa77e808f0aa99b2befa1ef20138a
Ashwikk <ashwikk045@gmail.com>
2024-07-05 13:45:11 +0530

    ans4

---
+++ ans4/ashwikk.js
@@ -0,0 +1,18 @@
+// Part 1: For Loop to Print Numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Part 2: While Loop to Print Numbers from 10 to 1
+let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+// Part 3: For Loop to Print Each Fruit from an Array
+const favoriteFruits = ["Apple", "Banana", "Mango", "Strawberry", "Pineapple"];
+
+for (let i = 0; i < favoriteFruits.length; i++) {
+    console.log(favoriteFruits[i]);
+}
